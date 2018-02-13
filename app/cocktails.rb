e91e2a75711613deefc997ce5a6d285ce9c06e2c
izzee <ichabon@me.com>
2018-02-13 11:10:35 -0500

    ghdsfd

--- app/cocktails.rb
+++ app/cocktails.rb
@@ -1,35 +1,11 @@
 class Cocktail
 
-  # def find_random_id
-  #   random_cocktail = RestClient.get('http://www.thecocktaildb.com/api/json/v1/1/random.php')
-  #   cocktail_hash = JSON.parse(random_cocktail)
-  # end
-  #
-  # def find_cocktail_hash
-  #   id_cocktail = RestClient.get("http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{random_cocktail_info}")
-  #   id_cocktail_hash = JSON.parse(id_cocktail)
-  # end
-  #
-  # def random_cocktail_info
-  #   self.find_random_id["drinks"][0]["idDrink"].to_s
-  # end
-  #
-  # def cocktail_info
-  #   self.find_cocktail_hash["drinks"][0]
-  # end
-  #
-  # def instructions
-  #   self.cocktail_info["strInstructions"]
-  # end
-
   @@id = ""
-
 
   def get_list_by_ingredient(alcohol)
     list_by_ingredient = RestClient.get("http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{alcohol}")
     @ingredient_hash = JSON.parse(list_by_ingredient)
   end
-
 
   def get_random_drink
     @ingredient_hash["drinks"].sample
@@ -53,7 +29,6 @@ class Cocktail
     cocktail_info = self.get_cocktail_from_id["drinks"][0]
   end
 
-  #
   def ingredients
     ingredients = []
     counter = 1
@@ -67,20 +42,4 @@ class Cocktail
     end
     ingredients
   end
-
-
-
-  #   counter = 1
-  #   ingredients = []
-  #   while  counter <= 15
-  #     if self.cocktail_info["strIngredient#{counter}"]
-  #       ingredients << self.cocktail_info["strMeasure#{counter}"].strip + ": " + self.cocktail_info["strIngredient#{counter}"]
-  #       counter +=1
-  #     else
-  #       break
-  #     end
-  #   end
-  #   ingredients
-  # end
-
 end
