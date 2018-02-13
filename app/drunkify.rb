5465bd13c127f8fd4c656d776955ccfda0d32173
izzee <ichabon@me.com>
2018-02-13 15:32:06 -0500

    drunkify

---
+++ app/drunkify.rb
@@ -0,0 +1,33 @@
+def shlur(string)
+  slurred = ['']
+  drunkenness = 2
+  split_by_letter = string.split('')
+  x = split_by_letter.map do |letter|
+    rando = rand(1..drunkenness)
+    if letter == 's' && rando == 1
+      letter += 'h'
+    else
+      letter
+    end
+  end
+  x.join('')
+end
+
+def burp(string)
+  drunkenness = 8
+  split_string = string.split(' ')
+  counter = 0
+  while counter < split_string.length-1
+    counter += 1
+    burp = rand(1..drunkenness)
+    if burp == 1
+      split_string[counter] << " burp!"
+    elsif burp == 10
+      split_string[counter] << " hic!"
+    end
+  end
+  puts split_string.join(' ')
+end
+
+x = shlur("Pour all ingredients directly into highball glass filled with ice. Stir gently. Garnish. Add a dash of Angostura bitters.")
+burp(x)
